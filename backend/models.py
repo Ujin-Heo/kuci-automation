f55117667fd26743e28f5b48ce1bea0fd8c238a4
howtojin <hujin2005@korea.ac.kr>
2024-12-05 11:12:06 +0900

    Board, Article model 생성함

--- backend/models.py
+++ backend/models.py
@@ -1,15 +1,42 @@
 from config import db
 
-class Contact(db.Model):
+
+# One-to-Many Relationship
+# Board - Article
+ 
+class Board(db.Model):
+    __tablename__ = 'boards'
+
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(80), unique=False, nullable=False) # 80은 maxlength
-    last_name = db.Column(db.String(80), unique=False, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    
+    # one-to-many relationship 설정하기
+    articles = db.relationship('Article', backref='board', lazy=True)
 
     def to_json(self):
         return {
             'id': self.id,
-            'firstName': self.first_name,
-            'lastName': self.last_name,
-            'email': self.email,
+            'name': self.name,
+            'articles': [article.to_json() for article in self.articles],
+        }
+    
+class Article:
+    __tablename__ = 'articles'
+
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String(10), unique=False, nullable=True) # 날짜가 없는 상단 고정 게시물을 어떻게 처리할지 아직 미정.
+    title = db.Column(db.String(100), unique=False, nullable=False)
+    # content = db.Column(db.) 나중에 지피티 기능 만들 때 추가할 거임
+    link = db.Column(db.String(100), unique=False, nullable=False)
+
+    # Board에 연결해주는 Foreign key
+    board_id = db.Column(db.Integer, db.ForeignKey('boards.id'), nullable=False)
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'date': self.date,
+            'title': self.title,
+            'link': self.link,
+            'board_id': self.board_id,
         }
